c8058fec8157ef9543496c4fe62ce6ac8d503116
YacineBoukliff <yacine69800@live.fr>
2025-01-05 12:26:33 +0100

    Factorisation et ajout de nouvelles fonctionnalités

--- react-exo/src/App.tsx
+++ react-exo/src/App.tsx
@@ -7,55 +7,59 @@ function App() {
     texte: string;
   }
 
+  interface Favoris {
+    id: number;
+    texte: string;
+  }
+
   const [texte, setTexte] = useState("");
-  const [todolist, setTodolist] = useState<Todolist[]>([
-    { id: 1, prix: 10, texte: "Je suis yacine" },
-    { id: 2, prix: 20, texte: "Je suis norhane" },
-    { id: 3, prix: 30, texte: "Je suis salah" },
-  ]);
+  const [todolist, setTodolist] = useState<Todolist[]>([]);
 
-  const changement = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTexte(e.target.value);
-  };
+  const [favoris, setFavoris] = useState<Favoris[]>([]);
 
-  const SupprimerElement = (id: number) => {
-    setTodolist(todolist.filter((todo) => todo.id !== id));
-  };
-
-  const total = todolist.reduce((acc, element) => acc + element.prix, 0);
-
-  const Moyenne =
-    todolist.reduce((acc, element) => acc + element.prix, 0) / todolist.length;
-
-  const ajouterElement = () => {
+  const GérerFavoris = () => {
     if (texte.trim() !== "") {
-      const NouvelElement: Todolist = {
-        id: todolist.length + 1,
-        prix: Math.floor(Math.random() * 100, ),
+      const id = todolist.length + 1;
+      const nouveauTodo = {
+        id: id,
+        prix: Math.floor(Math.random() * 100),
         texte: texte,
       };
-
-      setTodolist([...todolist, NouvelElement]);
+      
+      setTodolist([...todolist, nouveauTodo]);
+      setFavoris([...favoris, { id: id, texte: texte }]);
       setTexte("");
     }
   };
+
+  const SupprimerElement = (id: number) => {
+    setTodolist(todolist.filter(todo => todo.id !== id));
+    setFavoris(favoris.filter(fav => fav.id !== id));
+  };
+
+  const total = todolist.reduce((acc, element) => acc + element.prix, 0);
+  const Moyenne = todolist.length === 0 ? "Pas de moyenne disponible" : total / todolist.length;
+
+
 
   return (
     <>
       <input
         type="text"
         value={texte}
-        onChange={changement}
+        onChange={(e) => setTexte(e.target.value)}
         className="border-red-500 border-2"
       />
       <p>Ton texte est : {texte}</p>
       <button 
-  onClick={ajouterElement} 
-  disabled={total >= 100}
-  className={`rounded-2xl py-2 px-2 ${total >= 100 ? 'bg-gray-400' : 'bg-blue-400'}`}
->
-  Rajouter
-</button>
+        onClick={GérerFavoris} 
+        disabled={total >= 100}
+        className={`rounded-2xl py-2 px-2 ${total >= 100 ? 'bg-gray-400' : 'bg-blue-400'}`}
+      >
+        Rajouter
+      </button>
+
+      <p className="text-red-500"> {total >= 100 ? "Vous avez dépasser le maximum veuillez supprimer un élement " : ""} </p>
 
       {todolist.map((todo) => (
         <div key={todo.id} className="flex-col gap-x-5 mt-5">
@@ -63,17 +67,24 @@ function App() {
           <a> Mon prix est de {todo.prix} </a>
           <a>
             {todo.texte}{" "}
-            <button onClick={() => SupprimerElement(todo.id)}>
-              {" "}
-              Supprimer
-            </button>
+            <button onClick={() => SupprimerElement(todo.id)}>Supprimer</button>
           </a>
         </div>
       ))}
 
       <div className="mt-5">
-        <p> Le total de ce tableau est de : {total}</p>
-        <p> La moyenne de ce tableau est de : {Moyenne}</p>
+        <p>Le total : {total}</p>
+        <p>La moyenne : {Moyenne}</p>
+      </div>
+
+      <div className="mt-10">
+        <p>Favoris : {favoris.length}</p>
+        {favoris.map(fav => (
+          <div className="flex gap-x-5" key={fav.id}>
+            <p>{fav.id}</p>
+            <p>{fav.texte}</p>
+          </div>
+        ))}
       </div>
     </>
   );
